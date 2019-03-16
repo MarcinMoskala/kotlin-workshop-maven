33bef66b97a16ae76181c9c76c40a78d512b7cf2
Marcin Moskala <marcinmoskala@gmail.com>
2019-03-16 13:00:51 +0100

    Update

--- src/main/java/basics/Factorial.kt
+++ src/main/java/basics/Factorial.kt
@@ -2,4 +2,4 @@ package basics
 
 // factorial(5) = 5! = 5 * 4 * 3 * 2 * 1 = 120
 // https://en.wikipedia.org/wiki/Factorial
-tailrec fun factorial(n: Int, acc: Long = 1L): Long = if(n <= 1) acc else factorial(n - 1, acc * n)
+fun factorial(n: Int): Long = TODO()
--- src/main/java/basics/Tree.kt
+++ src/main/java/basics/Tree.kt
@@ -1,10 +1,22 @@
 package basics
 
-sealed class Tree {
-    override fun toString(): String = when (this) {
-        is Leaf -> value
-        is Node -> "$left, $right"
+abstract class Tree {
+    override fun toString(): String {
+        return treeToString(this, StringBuilder()).toString()
     }
 }
 data class Leaf(val value: String): Tree()
 data class Node(val left: Tree, val right: Tree): Tree()
+
+private fun treeToString(tree: Tree, sb: StringBuilder): StringBuilder {
+    if(tree is Leaf) {
+        val leaf = tree as Leaf
+        sb.append(leaf.value)
+    } else if(tree is Node) {
+        val node = tree as Node
+        treeToString(node.left, sb)
+        sb.append(", ")
+        treeToString(node.right, sb)
+    }
+    return sb
+}
